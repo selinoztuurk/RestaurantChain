b5c16b463d4d01fac25027a3bf804a26a423e3e3
Selin Öztürk <67735232+selinoztuurk@users.noreply.github.com>
2021-06-05 21:25:03 +0300

    styling completed

--- index_quiz.php
+++ index_quiz.php
@@ -1,87 +1,81 @@
 <html>
-<head></head>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="index_quiz.css">
+</head>
 <body>
 
+    <div class="ui container" id="main">
+    <h1 class="ui center aligned header">Welcome to our Restaurant Chain!</h1>
+    <img class="ui centered large image" src="restaurant.jpg">
+    <br>
+    <hr>
+
     <!-- Find the names of the recipes served in Branch1 but not served in Branch2 -->
+    
+    <h3 class="ui center aligned header">Our each branch offers a unique experience. See which recipes are served on one branch and not on the other, by entering the ID number of each branch. </h3>
+    
     <form action='result_quiz.php' method='post'>
-        <label>First Branch: </label><input type='number' name='first_branch' /><br/>
-        <label>Second Branch: </label><input type='number' name='second_branch' />
-        <input name="get_different_recipes", value='Get Different Recipes' type='submit'/></p>
+    <div class="ui input">
+        <input type="number" name="first_branch" placeholder="First Branch ID">
+    </div>
+    <div class="ui input">
+    <input type="number" name="second_branch" placeholder="Second Branch ID">
+    </div>
+        
+    <button class="ui button" name="get_different_recipes" type='submit'>
+        Get Different Recipes
+    </button>
     </form>
     <hr>
 
     <!-- Find the names of the staff that works in a branch with higher rating than 
         average of all braches and has served more than 20 orders -->
+    
+    <h3 class="ui center aligned header">We apprecieate all the efort that our staff has been putting in since the very first day. Click to view our best staff who has received above the average rating on their orders and has successfully served more than 20 times.</h3>
+    
     <form action='result_quiz.php' method='post'>
-        <input name="get_successful_staff", value='Get Successful Staff' type='submit'/></p>
+        <button class="ui button" name="get_successful_staff" type='submit'>
+        Get Successful Staff
+        </button>
     </form>
     <hr>
 
     <!-- Give them raise -->
-        <form action='result_quiz.php' method='post'>
-        <label>How much % raise do you want to give?: </label><input type='number' name='raise' /><br/>
-        <input name="raise_salaries", value='Raise Salaries' type='submit'/></p>
+    <h3 class="ui center aligned header">Customer is always right in our Restaurant Chain. Give our best staff a salary raise on any amount you wish.</h3>
+    
+    <form action='result_quiz.php' method='post'>
+
+    <div class="ui input">
+    <input type="number" name="raise" placeholder="How much % raise?">
+    </div>
+    <button class="ui button" name="raise_salaries" type='submit'>
+    Raise Salaries
+    </button>
     </form>
     <hr>
 
     <!-- Annual profit -->
+    <h3 class="ui center aligned header">If you are an investor, our Restaurant Chain is the best option you have! Check our annual profits by entering a year.</h3>
+    
     <form action='result_quiz.php' method='post'>
-        <label>Which year's profits would you like to see?: </label><input type='number' name='year' /><br/>
-        <input name="get_annual_profit", value='Get Annual Profit' type='submit'/></p>
+    <div class="ui input">
+    <input type="number" name="year" placeholder="Which year's profits?">
+    </div>
+    <button class="ui button" name="get_annual_profit" type='submit'>
+    Get Annual Profit
+    </button>
     </form>
     <hr>
 
     <!-- Find the average profit list of recipes -->
-        <form action='result_quiz.php' method='post'>
-        <input name="get_average_profits", value='Get Average Recipe Profits' type='submit'/></p>
-    </form>
-    <hr>
-
-
-    <!--Insert operation -->
+    <h3 class="ui center aligned header">Some of our recipes are not only super delicious, but also very profitable! Click to see the average profits receieved from each recipe.</h3>
+    
     <form action='result_quiz.php' method='post'>
-        <label>CID:         </label><input type='number' name='cid_insert' /><br/>
-        <label>Name:        </label><input type='text' name='name_insert' /><br/>
-        <label>CountryCode: </label><input type='text' name='country_code_insert' /><br/>
-        <label>District:    </label><input type='text' name='district_insert' /><br/>
-        <label>Population:  </label><input type='text' name='population_insert' /><br/>
-        <input name="insert", value='Insert' type='submit'/></p>
+    <button class="ui button" name="get_average_profits" type='submit'>
+    Get Average Recipe Profits
+    </button>
     </form>
-    <hr>
-
-    <!--Remove operation -->
-    <form action='result_quiz.php' method='post'>
-        <label>CID:</label><input type='number' name='cid_remove' /><input name="remove", value='Remove' type='submit'/></p>
-    </form>
-    <hr>
-
-    <!-- Manipulation operation -->
-    <form action='result_quiz.php' method='post'>
-        <label>Name: </label><input type='text' name='name_insert' /><br/>
-        <label>CID:</label><input type='number' name='cid_manipulate' /><input name="manipulate", value='Manipulate' type='submit'/></p>
-    </form>
-    <hr>
-
-    <!-- Please write Question 2 GUI Here -->
-    <form action='result_quiz.php' method='post'>
-        <label>First Country: </label><input type='text' name='first_country' /><br/>
-        <label>Second Country:</label><input type='text' name='second_country' /><input name="get_differences", value='Get Differences' type='submit'/></p>
-    </form>
-    <hr>
-
-    <!-- Please write Question 3 GUI Here -->
-    <form action='result_quiz.php' method='post'>
-        <label>First Country Code: </label><input type='text' name='first_country_code' /><br/>
-        <label>Second Country Code:</label><input type='text' name='second_country_code' /><input name="get_differences_join", value='Get Differences by Join' type='submit'/></p>
-    </form>
-    <hr>
-
-    <!-- Please write Question 4 GUI Here -->
-    <form action='result_quiz.php' method='post'>
-        <label>Choose an operation type: </label><select name='ops'><option value='MIN'>MIN</option><option value='AVG'>AVG</option></select><br/>
-        <label>Country Name:</label><input type='text' name='country_name' /><input name="aggregate_countries", value='Get Life Expectancy' type='submit'/></p>
-    </form>
-    <hr>
-
+    </div>
 </body>
 </html>
